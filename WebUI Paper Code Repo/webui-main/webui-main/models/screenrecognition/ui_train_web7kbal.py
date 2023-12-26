a3f0263d9b4aff4603e69e960e30fa393ff0f10f
IasonC <iasonchaimalas@gmail.com>
2023-12-26 03:18:26 +0000

    JADE job -- web7k-balanced training

---
+++ WebUI Paper Code Repo/webui-main/webui-main/models/screenrecognition/ui_train_web7kbal.py
@@ -0,0 +1,50 @@
+if __name__ == "__main__":
+
+    ARTIFACT_DIR = "./checkpoints_screenrecognition_web7kbal"
+
+    CHECK_INTERVAL_STEPS = 2000
+
+    import os
+
+    if not os.path.exists(ARTIFACT_DIR):
+        os.makedirs(ARTIFACT_DIR)
+
+    from ui_datasets import *
+    from ui_models import *
+    from pytorch_lightning import Trainer
+    from pytorch_lightning.callbacks import *
+    import torch
+    import datetime
+    from pytorch_lightning.loggers import TensorBoardLogger
+    logger = TensorBoardLogger(ARTIFACT_DIR)
+    
+    data = WebUIDataModule(train_split_file = '../../downloads/balanced_7k.json')
+    
+    model = UIElementDetector(num_classes=32)
+    
+    print("***********************************")
+    print("checkpoints: " + str(os.listdir(ARTIFACT_DIR)))
+    print("***********************************")
+    
+    
+    checkpoint_callback = ModelCheckpoint(dirpath=ARTIFACT_DIR, every_n_train_steps=CHECK_INTERVAL_STEPS, save_last=True)
+    checkpoint_callback2 = ModelCheckpoint(dirpath=ARTIFACT_DIR, filename= "screenrecognition",monitor='mAP', mode="max", save_top_k=1)
+    
+    earlystopping_callback = EarlyStopping(monitor="mAP", mode="max", patience=10)
+
+    trainer = Trainer(
+        gpus=1,
+        precision=16,
+        gradient_clip_val=1.0,
+        accumulate_grad_batches=2,
+        callbacks=[checkpoint_callback, checkpoint_callback2, earlystopping_callback],
+        val_check_interval=CHECK_INTERVAL_STEPS,
+        min_epochs=10,
+        logger=logger,
+        limit_val_batches=0.5
+    )
+    
+    if os.path.exists(os.path.join(ARTIFACT_DIR, "last.ckpt")):
+        model = UIElementDetector.load_from_checkpoint(os.path.join(ARTIFACT_DIR, "last.ckpt"))
+
+    trainer.fit(model, data)
