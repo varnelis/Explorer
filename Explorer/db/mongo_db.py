5ae48b9024fda6d7e5f9e84149622a946ede943e
ArnasVysniauskas <zceeavy@ucl.ac.uk>
2024-02-14 13:49:01 +0000

    add connection to mongo db

---
+++ Explorer/db/mongo_db.py
@@ -0,0 +1,25 @@
+
+from pymongo.mongo_client import MongoClient
+
+class MongoDBInterface:
+
+    username = "admin"
+    password = "uRQzyxGz95LxSEYW"
+    uri = "mongodb+srv://{username}:{password}@explorer-production.oh5gkiu.mongodb.net/?retryWrites=true&w=majority"
+    prod_db_name = "explorer-main"
+
+    client = None
+    database = None
+
+    @classmethod
+    def connect(cls, username: str = None, password: str = None):
+        login_uri = cls.uri(
+            password = password if password is not None else cls.password,
+            username = username if username is not None else cls.username,
+        )
+        cls.client = MongoClient(login_uri)
+        cls.database = cls.client[cls.prod_db_name]
+
+    @classmethod
+    def add_items(cls, items: list[dict], collection: str):
+        pass
