8e0a297ad2c13154d8a1bbc30ea10770a28284d9
ArnasVysniauskas <zceeavy@ucl.ac.uk>
2024-02-21 23:19:38 +0000

    add mongo db classes

--- Explorer/db/mongo_db.py
+++ Explorer/db/mongo_db.py
@@ -1,10 +1,10 @@
 
+from bson import ObjectId
+from pymongo.cursor import Cursor
 from pymongo.mongo_client import MongoClient
 
 class MongoDBInterface:
-    username = "admin"
-    password = "uRQzyxGz95LxSEYW"
-    uri = "mongodb+srv://{username}:{password}@explorer-production.oh5gkiu.mongodb.net/?retryWrites=true&w=majority"
+    uri = "mongodb+srv://admin:gJZY5tAn3USh5fuI@explorer-production.oh5gkiu.mongodb.net/?retryWrites=true&w=majority"
     prod_db_name = "explorer-main"
 
     client = None
@@ -12,13 +12,19 @@ class MongoDBInterface:
 
     @classmethod
     def connect(cls, username: str = None, password: str = None):
-        login_uri = cls.uri(
-            password = password if password is not None else cls.password,
-            username = username if username is not None else cls.username,
-        )
-        cls.client = MongoClient(login_uri)
+        cls.client = MongoClient(cls.uri)
         cls.database = cls.client[cls.prod_db_name]
 
     @classmethod
-    def add_items(cls, items: list[dict], collection: str):
-        pass
+    def add_items(cls, items: list[dict], collection: str) -> list[ObjectId]:
+        if collection not in cls.database.list_collection_names():
+            raise NameError(f"Colelction {collection} does not exist in the database")
+        result = cls.database[collection].insert_many(items)
+        return result.inserted_ids
+
+    @classmethod
+    def get_items(cls, filter: dict, collection: str) -> Cursor:
+        if collection not in cls.database.list_collection_names():
+            raise NameError(f"Colelction {collection} does not exist in the database")
+        cursor = cls.database[collection].find(filter)
+        return cursor
