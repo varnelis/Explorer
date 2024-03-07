ea11b22b32346c1781aa4d611be328d643cf70c8
uni-email <zceeavy@ucl.ac.uk>
2024-03-07 16:23:29 +0000

    Adjust shortlister to accept images instead of uuids

--- Explorer/overlay/shortlister.py
+++ Explorer/overlay/shortlister.py
@@ -12,7 +12,8 @@ from PIL import Image, ImageDraw
 from torchvision import transforms
 
 from Explorer.db.mongo_db import MongoDBInterface
-from Explorer.overlay.ui_models_distributed import *
+from Explorer.ocr.ocr import OCR
+from Explorer.overlay.ui_models_distributed import UIElementDetector
 
 import seaborn as sns
 import matplotlib.pylab as plt
@@ -23,28 +24,131 @@ def from_dict_list(_type, data: list) -> list:
 ShortlisterType = Literal["ocr", "interactable-detector", "web7kbal", "web350k", "vins"]
 InteractableModel = Literal["interactable-detector", "web7kbal", "web350k", "vins"]
 
+model2file = {
+    "web7kbal": "screenrecognition-web7kbal.ckpt",
+    "web350k": "screenrecognition-web350k.ckpt",
+    "vins": "screenrecognition-web350k-vins.ckpt",
+    "interactable-detector": "screenrecognition-interactdetect.ckpt",
+}
+
+model2version = {
+    "web7kbal": "v0.1.0",
+    "web350k": "v0.2.0",
+    "vins": "v0.3.0",
+    "interactable-detector": "v0.4.0",
+}
 
 class Shortlister:
-    def __init__(self, base_dir: str) -> None:
-        super(Shortlister).__init__()
+    def __init__(self) -> None:
+        if not os.path.exists("./shortlist_images/"):
+            os.makedirs("./shortlist_images/")
+        
+        if not os.path.exists("./weights"):
+            os.makedirs("./weights")
 
-        self.base_dir = base_dir
-        self.transforms = transforms.ToTensor()
-        self.shortlist_bbox = {
-            "ocr": [],
-            "interactable-detector": [],
-            "web7kbal": [],
-            "web350k": [],
-            "vins": [],
-        }
+        self.to_tensor = transforms.ToTensor()
 
-    def get_image_ocr_by_uuid(
+        self.bboxes: list | None = None
+        self.img_w_bboxes: Image.Image | None = None
+        self.model: ShortlisterType | None = None
+        self.img: Image.Image | None = None
+
+    def set_model(self, model: ShortlisterType) -> "Shortlister":
+        self.model = model
+        return self
+
+    def set_img(self, img: Image.Image) -> "Shortlister":
+        self.img = img
+        return self
+
+    def set_bboxes(self) -> "Shortlister":
+        if self.model is None:
+            raise IndexError("Model was not chosen, call Shortlister.set_model() first")
+        if self.img is None:
+            raise IndexError("Image was not loaded, call Shortlister.set_img() first")
+        
+        self.img_w_bboxes = None
+        if self.model == "ocr":
+            bboxes = self._get_bboxes_ocr()
+        else:
+            bboxes = self._get_bboxes_webui()
+        self.bboxes = bboxes
+
+        return self
+    
+    def show(self) -> "Shortlister":
+        if self.img_w_bboxes is None:
+            self._add_bboxes_to_img()
+        
+        self.img_w_bboxes.show()
+
+    def save(self) -> "Shortlister":
+        if self.img_w_bboxes is None:
+            self._add_bboxes_to_img()
+        
+        self.img_w_bboxes.save(f"./shortlist_images/{self.model}.png")
+
+    def _add_bboxes_to_img(self):
+        if self.bboxes is None:
+            raise IndexError("Bounding boxes were not genreted, call Shortlister.set_bboxes() first")
+
+        imgcpy = self.img.copy()  # copy as ImageDraw modifies inplace
+
+        draw_bbox = ImageDraw.Draw(imgcpy)
+        for bbox in self.bboxes:
+            draw_bbox.rectangle(bbox, outline="red", width=3)
+
+        self.img_w_bboxes = imgcpy
+
+    def _get_bboxes_webui(
+        self,
+        interactable_threshold: float = 0.5,
+        nms_iou_threshold: float = 0.2,
+    ) -> None:
+        """Shortlist based on an interactable detector model (ours or WebUI)
+        above confidence interactable_threshold (plus NMS up to nms_iou_threshold)."""
+
+        self._get_model_weights(self.model)
+        m = UIElementDetector.load_from_checkpoint(self.model_weights_path).eval()
+        
+        img_input = self.to_tensor(self.img.copy().convert("RGB"))
+        output_bbox_pred = m.model([img_input])
+        bbox_preds_all = output_bbox_pred[0]["boxes"].tolist()
+        bbox_scores = output_bbox_pred[0]["scores"].detach().numpy()
+
+        try:
+            # get last idx for boxes with confidence >= 0.5
+            last_valid_idx = np.where(bbox_scores < interactable_threshold + 1e-6)[0][0]
+            bboxes = bbox_preds_all[:last_valid_idx]
+        except IndexError:  # all bbox preds higher than threshold
+            bboxes = bbox_preds_all
+
+        return self._non_max_suppression(bboxes, nms_iou_threshold)
+    
+    def _get_bboxes_ocr(
+        self,
+        confidence_threshold: float = 0.5,
+        nms_iou_threshold: float = 0.2,
+    ) -> None:
+        """Shortlist based on all unranked OCR bbox above confidence threshold"""
+
+        _, ocr_bboxes, confidence = OCR.get_text(self.img)
+        bboxes = []
+
+        for c, bbox in zip(confidence, ocr_bboxes):
+            if c < confidence_threshold:
+                continue
+            bboxes.append(bbox)
+
+        return self._non_max_suppression(bboxes, nms_iou_threshold)
+
+    """def get_image_ocr_by_uuid(
         self,
         uuid: Union[str | UUID],
         show_image: bool,
         save_image: Union[str | None] = None,
     ) -> None:
-        """Image & OCR by querying MongoDB database for UUID"""
+        "Image & OCR by querying MongoDB database for UUID"
 
         MongoDBInterface.connect()
         screenshot = MongoDBInterface.get_items({"uuid": uuid}, "screenshots").limit(1)
@@ -61,106 +165,41 @@ class Shortlister:
         ocr_data = MongoDBInterface.get_items(
             {"screenshot_id": image_id}, "image-text-content"
         )
-        self.ocr_data = list(ocr_data)[0]
-
-    def get_model_weights(self, model: InteractableModel) -> None:
+        self.ocr_data = list(ocr_data)[0]"""
+    
+    def _get_model_weights(self, model: InteractableModel) -> None:
         """ Get file for weights of the model (local or from mongodb) """
         
-        self.model2file = {
-            "web7kbal": "screenrecognition-web7kbal.ckpt",
-            "web350k": "screenrecognition-web350k.ckpt",
-            "vins": "screenrecognition-web350k-vins.ckpt",
-            "interactable-detector": "screenrecognition-interactdetect.ckpt",
-        }
-        self.model2version = {
-            "web7kbal": "v0.1.0",
-            "web350k": "v0.2.0",
-            "vins": "v0.3.0",
-            "interactable-detector": "v0.4.0",
-        }
-        
         MongoDBInterface.connect()
-        weights = MongoDBInterface.get_items({"version": self.model2version[model]}, "detectors").limit(1)
+        weights = MongoDBInterface.get_items({"version": model2version[model]}, "detectors").limit(1)
         weights = list(weights)[0]
-        weights_url = weights["url"]#.split('/file/d/')[1]
-        
-        self.model_weights_path = self._gdown_url(weights_url, "weights", self.model2file[model])
 
-    def _gdown_url(self, url: str, model_path: str, model_key: str) -> None:
-        if not os.path.exists(os.path.join(self.base_dir, model_path)):
-            os.makedirs(os.path.join(self.base_dir, model_path))
-        if not os.path.exists(os.path.join(self.base_dir, model_path, model_key)):
+        self.model_weights_path = self._gdown_url(weights["url"], model2file[model])
+
+    def _gdown_url(self, url: str, model_key: str) -> None:
+        if not os.path.exists(os.path.join("./weights", model_key)):
             #url = url + '/view?usp=share_link'
             print('Retrieving weights from URL = ', url)
-            gdown.download(url=url, output=os.path.join(self.base_dir, model_path, model_key), fuzzy=True)
-        return os.path.join(self.base_dir, model_path, model_key)
+            gdown.download(url=url, output=os.path.join("./weights", model_key), fuzzy=True)
+        return os.path.join("./weights", model_key)
 
-    def shortlist_ocr(
-        self,
-        confidence_threshold: float = 0.5,
-        nms_iou_threshold: float = 0.2,
-    ) -> None:
-        """Shortlist based on all unranked OCR bbox above confidence threshold"""
-
-        confidence = self.ocr_data["confidence"]
-        bbox_location = self.ocr_data["text_location"]
-
-        for i in range(len(confidence)):
-            if confidence[i] > confidence_threshold:
-                bbox = bbox_location[i]
-                xmin = bbox[0][0]
-                xmax = bbox[1][0]
-                ymin = bbox[0][1]
-                ymax = bbox[1][1]
-
-                self.shortlist_bbox["ocr"].append((xmin, ymin, xmax, ymax))
-
-        self._non_max_suppression("ocr", nms_iou_threshold)
-
-    def shortlist_interactables(
-        self,
-        model: InteractableModel,
-        interactable_threshold: float = 0.5,
-        nms_iou_threshold: float = 0.2,
-    ) -> None:
-        """Shortlist based on an interactable detector model (ours or WebUI)
-        above confidence interactable_threshold (plus NMS up to nms_iou_threshold)."""
-
-        self.get_model_weights(model)
-        m = UIElementDetector.load_from_checkpoint(self.model_weights_path).eval()
-        
-        img_input = self.transforms(self.image.copy().convert("RGB"))
-        output_bbox_pred = m.model([img_input])
-        bbox_preds_all = output_bbox_pred[0]["boxes"].tolist()
-        bbox_scores = output_bbox_pred[0]["scores"].detach().numpy()
-
-        try:
-            last_valid_idx = np.where(bbox_scores < interactable_threshold + 1e-6)[0][
-                0
-            ]  # get last idx for boxes with confidence >= 0.5
-            self.shortlist_bbox[model] = bbox_preds_all[:last_valid_idx]
-        except IndexError:  # all bbox preds higher than threshold
-            self.shortlist_bbox[model] = bbox_preds_all
-
-        self._non_max_suppression(model, nms_iou_threshold)
-
-    def _non_max_suppression(self, model: ShortlisterType, iou_threshold: float = 0.2):
+    def _non_max_suppression(self, bboxes, iou_threshold: float = 0.2):
         """NMS for bbox predictions on image. Drops low-confidence bbox with IoU overlap > threshold"""
 
         keep_bbox = []
         # self.shortlist_bbox and bbox_confidences are already sorted
-        while self.shortlist_bbox[model]:
-            i = self.shortlist_bbox[model].pop(0)
-            keep_bbox.append(i)
+        while len(bboxes) != 0:
+            bbox_1 = bboxes.pop(0)
+            keep_bbox.append(bbox_1)
             removals = []
-            for j in self.shortlist_bbox[model]:
-                iou = self._iou(bbox1=i, bbox2=j)
+            for bbox_2 in bboxes:
+                iou = self._iou(bbox_1, bbox_2)
                 if iou > iou_threshold:
-                    removals.append(j)
-            for j in removals:
-                self.shortlist_bbox[model].remove(j)
+                    removals.append(bbox_2)
+            for bbox in removals:
+                bboxes.remove(bbox)
 
-        self.shortlist_bbox[model] = keep_bbox
+        return keep_bbox
 
     def _iou(self, bbox1, bbox2):
         """Get IoU of 2 bbox of format (xmin, ymin, xmax, ymax)"""
@@ -178,53 +217,3 @@ class Shortlister:
         union = area1 + area2 - intersection
 
         return intersection / union
-
-    def draw_bbox(
-        self, model: ShortlisterType, show_image: bool, save_image: Union[str | None]
-    ):
-        if len(self.shortlist_bbox[model]) == 0:
-            raise Exception("bbox passed is empty. Shortlisting not yet performed.")
-
-        imgcpy = self.image.copy()  # copy as ImageDraw modifies inplace
-
-        draw_bbox = ImageDraw.Draw(imgcpy)
-        for i, bbox in enumerate(self.shortlist_bbox[model]):
-            draw_bbox.rectangle(bbox, outline="red", width=3)
-
-        if show_image:
-            imgcpy.show()
-        if save_image:
-            imgcpy.save(os.path.join(self.base_dir, "./shortlist_images/" + save_image + ".png"))
-
-    def get_shortlist(
-        self,
-        uuid: str,
-        shortlist_method: ShortlisterType,
-        shortlist_threshold: float = 0.5,
-        nms_iou_threshold: float = 0.2,
-        show_image: bool = False,
-        save_image: bool = False,
-    ) -> list[list[int | float]]:
-        """Get shortlist of interactable bbox to show, based on shortlisting method"""
-        if not os.path.exists("./shortlist_images/"):
-            os.makedirs("./shortlist_images/")
-        self.get_image_ocr_by_uuid(uuid, show_image, save_image)
-        if shortlist_method == "ocr":
-            self.shortlist_ocr(
-                confidence_threshold=shortlist_threshold,
-                nms_iou_threshold=nms_iou_threshold,
-            )
-        else:
-            self.shortlist_interactables(
-                model=shortlist_method,
-                interactable_threshold=shortlist_threshold,
-                nms_iou_threshold=nms_iou_threshold,
-            )
-        if show_image or save_image:
-            if save_image:
-                save_name = f"image_shortlist_{shortlist_method}_{uuid}"
-            else:
-                save_name = None
-            self.draw_bbox(shortlist_method, show_image, save_name)
-
-        return self.shortlist_bbox[shortlist_method]
